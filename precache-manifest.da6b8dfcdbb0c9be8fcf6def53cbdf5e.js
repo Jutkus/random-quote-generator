3e6de6c1a412d1c1ec0ec00cf40c1783c36f9ce6
Jutkus <63132049+Jutkus@users.noreply.github.com>
2020-08-07 20:53:09 -0500

    Updates

---
+++ precache-manifest.da6b8dfcdbb0c9be8fcf6def53cbdf5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf77e51a1dec6efe4bc7ad2fd81110a7",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "62b3ffc2245b076c3913",
+    "url": "/random-quote-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "525e4e8c6589afee802d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b3ffc2245b076c3913",
+    "url": "/random-quote-generator/static/js/main.56889b41.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  }
+]);
