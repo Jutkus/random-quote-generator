bc24ba19a2347eb4e7118cae92095f2554bc07de
Jutkus <63132049+Jutkus@users.noreply.github.com>
2020-08-07 21:13:44 -0500

    UNDO

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Quotes from "./Quotes";
 function App() {
   return (
     <div className="App" id="quote-box">
-    <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
       <Quotes />
     </div>
   );
