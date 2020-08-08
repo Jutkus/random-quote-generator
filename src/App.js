1952616ebd00aa857898b57ae84156cca2c78113
Jutkus <63132049+Jutkus@users.noreply.github.com>
2020-08-07 21:12:11 -0500

    Added FCC Script

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Quotes from "./Quotes";
 function App() {
   return (
     <div className="App" id="quote-box">
+    <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
       <Quotes />
     </div>
   );
